3464b05536c4ae6d1fc698da4b43a674439439af
richmond gozarin <ralph.gozarin@cloudsherpas.com>
2015-10-06 00:38:13 +0800

    service accounts

--- app/settings.py
+++ app/settings.py
@@ -19,6 +19,7 @@ settings['twitter'] = {
 
 # The Developer Console will give you a JSON file when creating a service account. You can either load it or paste it into here.
 
+# TODO
 # import json
 # with open('app/service_account.json') as f:
 #     settings['oauth2_service_account'] = json.load(f)
