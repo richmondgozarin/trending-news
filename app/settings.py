850f56ed67090c5cc7973cb9b523530df89a6419
richmond gozarin <ralph.gozarin@cloudsherpas.com>
2015-10-05 22:33:04 +0800

    account settings

--- app/settings.py
+++ app/settings.py
@@ -6,6 +6,16 @@ settings = {}
 # OAuth2 Configuration
 #
 
+#
+# Twitter Configuration
+#
+settings['twitter'] = {
+    'consumer_key': 'c1VGZBN8marmxwviK24SHsLB9',
+    'consumer_secret': 'QT7cckhEGfP5FgK5l3oxSWiPx5cHTcdjqWg41h12rrudxZlUyP',
+    'access_key': '66599089-TwaoP9jZGVj9qkS79eveKYvJVQaReeiWiHttd9EpL',
+    'access_secret': 'JTtEAyTGRSya46u20MPp3i1fnIApTarh5kSwpkxE3LBHp'
+
+}
 
 # The Developer Console will give you a JSON file when creating a service account. You can either load it or paste it into here.
 
