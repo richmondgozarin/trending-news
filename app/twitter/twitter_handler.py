eb3b7571933971b26df0f5be58cfe4dbbcb6e191
richmond gozarin <ralph.gozarin@cloudsherpas.com>
2015-10-06 00:24:36 +0800

    twitter handler worldwide trends

--- app/twitter/twitter_handler.py
+++ app/twitter/twitter_handler.py
@@ -1,20 +1,20 @@
 import webapp2
 from ferris3 import settings
 from twitter import Twitter, OAuth
+import logging
 
 config = settings.get("twitter")
 
 
 class TwitterHandler(webapp2.RequestHandler):
     def get(self):
-        twitter = Twitter(auth=OAuth(config["access_key"], config["access_secret"], config["consumer_key"], settings["consumer_secret"]))
+        twitter = Twitter(auth=OAuth(config["access_key"], config["access_secret"], config["consumer_key"], config["consumer_secret"]))
         results = twitter.trends.place(_id=1)
 
-        self.response.write("Worldwide Trends")
-
+        self.response.write("Worldwide Trends <br/>")
         for location in results:
             for trend in location["trends"]:
-                self.response.write(" - %s" % trend["name"])
+                self.response.write(" - %s <br/>" % trend["name"])
 
 
 # Ferris will automatically discover these routes
